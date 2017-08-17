f6cd87a381365b37dd4732f94390d009ae0c4e0e
miketak <mtakrama@yahoo.com>
2017-08-16 19:44:17 -0500

    Deployment of Version 1.0

--- SCCL.Infrastructure/DbConnection.cs
+++ SCCL.Infrastructure/DbConnection.cs
@@ -6,7 +6,8 @@ namespace SCCL.Infrastructure
     {
         internal static SqlConnection GetConnection()
         {
-            const string connString = @"Data Source=localhost;Initial Catalog=SCSYSDB;Integrated Security=True";
+            //const string connString = @"Data Source=localhost;Initial Catalog=SCSYSDB;Integrated Security=True";
+            const string connString = @"Data Source=184.168.47.15;Integrated Security=False;User ID=mtakrama;Password=Mtstap43@1;Connect Timeout=15;Encrypt=False;Packet Size=4096";
             //const string connString = @"Server=tcp:scsystem.database.windows.net,1433;Initial Catalog=scsysdb;Persist Security Info=False;User ID=mtakrama;Password=password@1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             var conn = new SqlConnection(connString);
             return conn;
